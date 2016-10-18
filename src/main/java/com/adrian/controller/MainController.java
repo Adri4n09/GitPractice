3de15dc8c8a2f67600609698ccc0141fe3dd12d5
Adri4n09 <imihai.adrian@gmail.com>
2016-10-19 00:00:37 +0300

    abc

--- src/main/java/com/adrian/controller/MainController.java
+++ src/main/java/com/adrian/controller/MainController.java
@@ -1,11 +1,9 @@
 package com.adrian.controller;
 
 import com.adrian.model.User;
-import com.adrian.model.User;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
